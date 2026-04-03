8b3117243e46a7316e62a006da8504179eedb180
Bruce Lewis <b.r.lewis@outlook.com>
2026-04-03 10:17:59 -0500

    Updated local variables

--- src/server.js
+++ src/server.js
@@ -127,9 +127,7 @@ const pool = mysql.createPool({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  port: process.env.MYSQLPORT
 });
 
 async function loadActiveUsers() {
