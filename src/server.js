8a401bac0152a57df77e2352734c767af796bd57
Bruce Lewis <b.r.lewis@outlook.com>
2026-04-03 10:35:33 -0500

    Verify variables

--- src/server.js
+++ src/server.js
@@ -123,11 +123,11 @@ app.get('/setup-db', async (req, res) => {
 });
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT || 3306)
 });
 
 async function loadActiveUsers() {
